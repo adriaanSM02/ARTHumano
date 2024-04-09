f7e34275e00a17b94f0fc526ccc7a54fbe0a0e00
ale-serrano <loli.doremi@gmail.com>
2024-04-08 20:36:55 -0600

    Parcial Inicio y Calendario
    Se implementa parte del modelo y datos de prueba en pantalla de inicio, mientras que se limita a la inclusión del widget de CalendarView en uno de los fragments

---
+++ app/src/main/java/com/example/arthumano_Consultores/ViewPagerItem.java
@@ -0,0 +1,13 @@
+package com.example.arthumano_Consultores;
+
+public class ViewPagerItem {
+
+    int imageID;
+    String heading, description;
+
+    public ViewPagerItem(int imageID, String heading, String description) {
+        this.imageID = imageID;
+        this.heading = heading;
+        this.description = description;
+    }
+}
